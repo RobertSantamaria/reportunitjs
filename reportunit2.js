7291efdd100a1d415a21d2f819d31a68307622f9
roberto-santamaria <santamaria.roberto@gmail.com>
2017-07-23 21:46:30 +0100

    New changes

--- reportunit2.js
+++ reportunit2.js
@@ -1,15 +1,37 @@
 var menuWidth = 260;
 
-function showDynamicModal(heading, content) {
-    var m = $('.modal-trigger');
-    document.getElementById('modal-title').innerHTML = heading;
-    document.getElementById('modal-message').innerHTML = content;
+$(document).ready(function() {
+    /* init */
+    showTestList();
+    drawPercentageChart();
+    testsChart();
+    
+    $(".collapsible").collapsible();
+});
+
+function showTestList() {
+    $(".details-container").html("");
+    var numberOfSuites = $("#suite-collection .suite-content");
+    for (i = 0; i < numberOfSuites.length; i++){
+        var suiteName = "#suite" + parseInt(i+1);
+        var html = $("#suite-collection").find(suiteName).html();    
+        $(".details-container").append(html);
+        $(".details-container ul.collapsible").addClass("tests");
+        $(".details-container .suite-content").removeClass("hide");
+    }
 }
 
-$('.details-container').click(function(evt) {
-    var t = $(evt.target);
-    showDynamicModal(t.closest('tr').find('.name').text() + ' StatusMessage: ', t.next().text());
-});
+function drawPercentageChart() {
+    var total = $(".tests .test-name").length;
+    var passed = $(".tests .test-name.passed").length;
+    var failed = $(".tests .test-name.failed").length;
+    var errors = $(".tests .test-name.error").length;
+    var skipped = $(".tests .test-name.skipped").length;
+
+    var passedPercentage = Math.round(((passed / total) * 100)) + "%";
+    $(".pass-percentage").text(passedPercentage);
+    $(".dashboard .determinate").attr("style", "width:" + passedPercentage);
+}
 
 /* show suite data on click */
 $('.suite').click(function() {
@@ -29,8 +51,12 @@ $('#slide-out .report-item > a').filter(function(){
     return this.href.match(/[^\/]+$/)[0] == document.location.pathname.match(/[^\/]+$/)[0];
 }).parent().addClass('active');
 
+$(".btn .clear").click(function() {
+    var t = $(this);
+});
+
 /* filters -> by suite status */
-$('#suites-dropdown li').click(function() {
+$('#suites-dropdown').click(function() {
     var t = $(this);
     
     if (!t.hasClass('clear')) {
@@ -90,40 +116,6 @@ function clickListItem(listClass, index) {
     $('#' + listClass).find('li').get(index).click();
 }
 
-$(document).ready(function() {
-	/* init */
-	$('select').material_select();
-	$('.tooltipped').tooltip({delay: 10});
-
-	var passedPercentage = Math.round(((passed / total) * 100)) + '%';
-	$('.pass-percentage').text(passedPercentage);
-	$('.dashboard .determinate').attr('style', 'width:' + passedPercentage);
-
-    testsChart();
-
-	//resetFilters();
-	//$('.suite:first-child').click();
-});
-
-var options = {
-	segmentShowStroke : false, 
-	percentageInnerCutout : 55, 
-	animationSteps : 1,
-	legendTemplate : '<ul class=\'<%=name.toLowerCase()%>-legend\'><% for (var i=0; i<segments.length; i++){%><li><span style=\'background-color:<%=segments[i].fillColor%>\'></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
-};
-
-/* test case counts */
-var total = $('.test-name').length;
-/*var passed = $('td.passed').length;
-var failed = $('td.failed').length;
-var errors = $('td.error').length;
-var skipped = $('td.skipped').length;
-*/
-var passed = 3;
-var failed = 2;
-var errors = 1;
-var skipped = 1;
-
 /* report -> tests chart */
 function testsChart() {
 	if (!$('#test-analysis').length) {
@@ -136,7 +128,7 @@ function testsChart() {
         datasets: [
             {
                 label: "My First Dataset",
-                data: [passed, failed, errors, skipped],
+                data: [1, 1, 0, 1],
                 backgroundColor: [
                     "#66bb6a",
                     "#ef5350",
@@ -149,35 +141,7 @@ function testsChart() {
     var options = {
         responsive: true,
         maintainAspectRatio: false,
-/*        tooltips: {
-            callbacks: {
-                label: function (tooltipItem, data) {
-                    return data.labels[tooltipItem.index] + ' (' + data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index] + '%)';
-                }
-            }
-        },
-        legendCallback: function (chart) {
-            var text = [];
-            text.push('<ul class="legend">');
-
-            var data = chart.data;
-            var datasets = data.datasets;
-            var labels = data.labels;
-
-            if (datasets.length) {
-                for (var i = 0; i < datasets[0].data.length; ++i) {
-                    text.push('<li><span style="background-color:' + datasets[0].backgroundColor + '">' + labels[i] + '</span>');
-                    if (labels[i]) {
-                        text.push(labels[i] + ' (' + datasets[0].data[i] + '%)');
-                    }
-                    text.push('</li>');
-                }
-            }
-            text.push('</ul>');
-            return text.join('');
-        },*/
         legend: {
-            // since you're providing your own legend
             display: true,
             position: "right"
         }
@@ -188,48 +152,4 @@ function testsChart() {
         data: data,
         options: options
     });
-
-    //var legend = myPieChart.generateLegend();
-    //document.getElementById("legend").innerHTML = legend;
-//drawLegend(myPieChart, "test-analysis")
-/*
-    var data = {};
-    
-    if ($('body.summary').length > 0) {
-        total = parseInt($('.total-tests').text());
-        passed = parseInt($('.total-passed').text());
-        failed = parseInt($('.total-failed').text());
-        others = parseInt($('.total-skipped').text());
-        
-        data = [
-            { value: passed, color: '#00af00', highlight: '#32bf32', label: 'Pass' },
-            { value: failed, color:'#F7464A', highlight: '#FF5A5E', label: 'Fail' },
-            { value: others, color: '#1e90ff', highlight: '#4aa6ff', label: 'Others' }
-        ];
-        
-        $('.test-others-count').text(others);
-    }
-    else {
-        data = [
-            { value: passed, color: '#00af00', highlight: '#32bf32', label: 'Pass' },
-            { value: failed, color:'#F7464A', highlight: '#FF5A5E', label: 'Fail' },
-            { value: errors, color:'#ff6347', highlight: '#ff826b', label: 'Error' },
-            { value: skipped, color: '#1e90ff', highlight: '#4aa6ff', label: 'Skip' }
-        ];
-        
-        $('.test-others-count').text(errors + skipped);
-    }
-	
-	$('.test-pass-count').text(passed);
-	$('.test-fail-count').text(failed);
-    
-*/
-    /*
-	var ctx = $("#test-analysis").get(0).getContext("2d");
-	testChart = new Chart(ctx, {
-        type: 'pie',
-        data: data,
-        options: options
-    });
-	drawLegend(testChart, 'test-analysis');*/
 }
